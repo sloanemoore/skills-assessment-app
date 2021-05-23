c5c7e9c926f6960e5540360f81ff173f168ba674
sloanemoore <61360034+sloanemoore@users.noreply.github.com>
2021-05-22 19:46:55 -0500

    responsive layout

--- progress-tracker-app/src/App.js
+++ progress-tracker-app/src/App.js
@@ -1,5 +1,6 @@
 import React, { useState, useEffect } from "react";
 import "bootstrap/dist/css/bootstrap.min.css";
+import Container from 'react-bootstrap/Container'
 import "./App.css";
 import SectionBox from "./SectionBox.js";
 import Header from "./Header.js";
@@ -166,14 +167,16 @@ function App() {
 
   return (
     <div className="App">
-      <Header handleAddSectionButtonClick={handleAddSectionButtonClick} />
-      <SectionBox
-        progressList={progressList}
-        setProgressList={setProgressList}
-        handleAddSkillButtonClick={handleAddSkillButtonClick}
-        handleDeleteSectionButtonClick={handleDeleteSectionButtonClick}
-        handleDeleteSkillButtonClick={handleDeleteSkillButtonClick}
-      />
+      <Container>
+        <Header handleAddSectionButtonClick={handleAddSectionButtonClick} />
+        <SectionBox
+          progressList={progressList}
+          setProgressList={setProgressList}
+          handleAddSkillButtonClick={handleAddSkillButtonClick}
+          handleDeleteSectionButtonClick={handleDeleteSectionButtonClick}
+          handleDeleteSkillButtonClick={handleDeleteSkillButtonClick}
+        />
+      </Container>
     </div>
   );
 }
